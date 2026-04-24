e26218d86ad7c6165c8973ee657abd2e116d1276
Hardik Dhwan <dhawanhardik180@gmail.com>
2026-04-24 13:53:43 +0530

    Update project

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -5,7 +5,11 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  (typeof window !== "undefined" && window.location.hostname !== "localhost"
+    ? "https://studymateai-one.vercel.app/_/backend"
+    : "http://localhost:8000");
 
 const api = axios.create({
   baseURL: BASE_URL,
